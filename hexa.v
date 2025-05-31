c03cba4bb11d6e38b756b2cd6e420f45c06651e0
Linklancien <linklancien@gmail.com>
2025-05-31 11:13:46 +0200

    Update hexa.v

--- hexa.v
+++ hexa.v
@@ -129,15 +129,16 @@ pub fn distance_hexa_y(x int, y int, new_x int, new_y int) int {
 	return distance_hexa_x(y, x, new_y, new_x)
 }
 
-pub fn path_to_hexa_x(x int, y int, new_x int, new_y int, max_x int, max_y int) [][2]int {
-	mut path := [][2]int{}
+pub fn path_to_hexa_x(x int, y int, new_x int, new_y int, max_x int, max_y int) [][]int {
+	mut path := [][]int{}
 	new_pos_x, new_pos_y := coo_hexa_x_to_ortho(new_x, new_y)
 	pos_x, pos_y := coo_hexa_x_to_ortho(new_x, new_y)
 	prec := 100
 	rise := (new_pos_y - pos_y) / prec
 	run := (new_pos_x - pos_x) / prec
 	for i in 0 .. prec {
-		test_x, test_y := coo_ortho_to_hexa_x(pos_x + i * run, pos_y + i * rise, max_x, max_y)
+		test_x, test_y := coo_ortho_to_hexa_x(pos_x + i * run, pos_y + i * rise, max_x,
+			max_y)
 		if [test_x, test_y] !in path {
 			path << [[test_x, test_y]]
 		}

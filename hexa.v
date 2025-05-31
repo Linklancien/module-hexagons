376d342a6b6a2b554e6addb218bc0b8f81060aba
Linklancien <linklancien@gmail.com>
2025-05-31 10:52:21 +0200

    Update hexa.v

--- hexa.v
+++ hexa.v
@@ -117,16 +117,32 @@ pub fn coo_ortho_to_hexa_y(x f32, y f32, max_x int, max_y int) (int, int) {
 
 // dist
 pub fn distance_hexa_x(x int, y int, new_x int, new_y int) int {
-	if new_x < x && y != new_y{
-		println(' < ${(x + 1 - new_x)}, ${(y - new_y)}, ${(x + 1 - new_x)*(x + 1 - new_x)}, ${(y - new_y)*(y - new_y)}')
+	if new_x < x && y != new_y {
+		println(' < ${(x + 1 - new_x)}, ${(y - new_y)}, ${(x + 1 - new_x) * (x + 1 - new_x)}, ${(y - new_y) * (y - new_y)}')
 		return int(sqrt((y - new_y) * (y - new_y) + (x + 1 - new_x) * (x + 1 - new_x)))
 	}
-	println(' > ${(x - new_x)}, ${(y - new_y)}, ${(x - new_x)*(x - new_x)}, ${(y - new_y)*(y - new_y)}')
+	println(' > ${(x - new_x)}, ${(y - new_y)}, ${(x - new_x) * (x - new_x)}, ${(y - new_y) * (y - new_y)}')
 	return int(sqrt((y - new_y) * (y - new_y) + (x - new_x) * (x - new_x)))
 }
 
 pub fn distance_hexa_y(x int, y int, new_x int, new_y int) int {
 	return distance_hexa_x(y, x, new_y, new_x)
+}
+
+pub fn path_to_hexa_x(x int, y int, new_x int, new_y int) [][]int {
+	mut path := [][2]int{}
+	new_pos_x, new_pos_y := coo_hexa_x_to_ortho(new_x, new_y)
+	pos_x, pos_y := coo_hexa_x_to_ortho(new_x, new_y)
+	prec := 100
+	rise := (new_pos_y - pos_y) / prec
+	run := (new_pos_x - pos_x) / prec
+	for i in 0 .. prec {
+		test_x, test_y := coo_ortho_to_hexa_x(pos_x + i * run, pos_y + i * rise)
+		if [test_x, test_y] !in path {
+			path << [test_x, test_y]
+		}
+	}
+	return path
 }
 
 // neighbors

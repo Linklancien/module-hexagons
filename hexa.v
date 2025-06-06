cd2ce4637e5b9297928f8aad40ed335f89e4453d
Linklancien <linklancien@gmail.com>
2025-06-06 17:15:00 +0200

    Update hexa.v

--- hexa.v
+++ hexa.v
@@ -52,15 +52,15 @@ pub fn coo_ortho_to_hexa_x(pos_x f32, pos_y f32, max_x int, max_y int) (int, int
 	// Search coo_x:
 	mut coo_x := -1
 	for x_test in 0 .. max_x {
-		if pos_x < 0.87 * 2 * x_test {
-			if 0.87 * (2 * x_test - 1) < pos_x {
+		if pos_x <= 0.87 * 2 * x_test {
+			if 0.87 * (2 * x_test - 1) <= pos_x {
 				coo_x = x_test
 				not_sure = true
 				break
 			}
 		} else {
 			// pos_x > 0.87*2*x_test
-			if pos_x < 0.87 * (2 * x_test + 1) {
+			if pos_x <= 0.87 * (2 * x_test + 1) {
 				coo_x = x_test
 				not_sure = false
 				break
